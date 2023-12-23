fbccb2b94c945db238bc40a571fbcddac7faacf9
Lokesh Ravindran <lokeshravindran3003@gmail.com>
2023-12-23 18:33:21 +0530

    Route basename added as per repo

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={"/authentication-mern"}>
         <App />
         <ToastContainer />
       </BrowserRouter>
