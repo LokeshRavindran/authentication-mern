552677b6f6983c9e9c18312ca32023097fdb6381
Lokesh Ravindran <lokeshravindran3003@gmail.com>
2023-12-23 08:13:51 +0530

    Backend url changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:4000";
-export const BASE_URL = "https://rose-wild-indri.cyclic.app";
+export const BASE_URL = "https://authentication-mern.cyclic.app/";
 
 export const EXCLUDE_HEADER = ["/login", "/signup"];
